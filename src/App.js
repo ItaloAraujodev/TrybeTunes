5a1e7f3c50026e90058d0005aeaa4497d5e8212f
Italodevjr <italoarauju2019@gmail.com>
2022-07-01 19:22:54 -0300

    Desafio 2 completo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,78 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Content from './pages/Content';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
+import Favorites from './pages/Favorites';
+import { createUser } from './services/userAPI';
+import Loading from './pages/Loading';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nameInput: '',
+      dissabledButton: true,
+      loading: false,
+      search: false,
+    };
+  }
+
+    buttonValid = () => {
+      const numMax = 3;
+      const { nameInput } = this.state;
+      const stringValid = nameInput.length >= numMax;
+
+      this.setState({
+        dissabledButton: !stringValid,
+      });
+    }
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.buttonValid());
+  }
+
+  createUser = async () => {
+    const { nameInput } = this.state;
+    this.setState({ loading: true },
+      async () => {
+        await createUser({ name: nameInput });
+        this.setState({ loading: false, search: true });
+      });
+  }
+
   render() {
+    const { dissabledButton, loading, search } = this.state;
     return (
       <BrowserRouter>
+        {loading ? <Loading /> : null }
+        {search ? <Redirect to="/search" /> : null}
         <div>
           <p>TrybeTunes</p>
-          <Content />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={ () => (
+                <Login
+                  onInputChange={ this.onInputChange }
+                  dissabled={ dissabledButton }
+                  buttonClick={ this.createUser }
+                />) }
+            />
+            <Route path="/search" component={ Search } />
+            <Route path="/album" component={ Album } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="/profile" component={ Profile } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
         </div>
       </BrowserRouter>
     );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
   render() {
+    const { dissabled, onInputChange, buttonClick } = this.props;
+
     return (
-      <div data-testid="page-login">
+      <div className="login-container" data-testid="page-login">
         <h2>login</h2>
         <div>
-          <input type="text" />
-          <button type="button">Entrar</button>
+          <label htmlFor="input-text">
+            <input
+              data-testid="login-name-input"
+              name="nameInput"
+              id="input-text"
+              type="text"
+              onChange={ onInputChange }
+            />
+          </label>
+
+          <button
+            data-testid="login-submit-button"
+            type="button"
+            disabled={ dissabled }
+            onClick={ buttonClick }
+          >
+            Entrar
+          </button>
         </div>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  dissabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  buttonClick: PropTypes.func.isRequired,
+};
 
 export default Login;
