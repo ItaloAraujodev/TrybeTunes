774cd26960696f92d45ee54f55f9e3d273b1d34a
Italodevjr <italoarauju2019@gmail.com>
2022-07-04 18:58:23 -0300

    Desafio 5 completo

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,46 @@ import React from 'react';
 import Header from '../Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      dissabled: true,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const numMax = 2;
+    const { name, value } = target;
+    this.setState({ [name]: value });
+    if (value.length >= numMax) {
+      this.setState({
+        dissabled: false,
+      });
+    } else {
+      this.setState({ dissabled: true });
+    }
+  }
+
   render() {
+    const { dissabled, input } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h2>Search</h2>
+        <input
+          name="input"
+          value={ input }
+          data-testid="search-artist-input"
+          type="text"
+          onChange={ this.onInputChange }
+        />
+        <button
+          disabled={ dissabled }
+          data-testid="search-artist-button"
+          type="button"
+        >
+          Procurar
+        </button>
       </div>
     );
   }
