6ad0588045c42a5b80616124d3663dfa56d3c707
Italodevjr <italoarauju2019@gmail.com>
2022-07-06 15:37:01 -0300

    Desafio 8 completo

--- src/pages/MusicCard.jsx
+++ src/pages/MusicCard.jsx
@@ -1,18 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      favotirs: {},
+      check: false,
+    };
+  }
+
+  onInputChecked = async () => {
+    const { favotirs } = this.state;
+    this.setState({ loading: true });
+    const song = await addSong(favotirs);
+    this.setState({
+      favotirs: song,
+      loading: false,
+      check: !check.value,
+    });
+  };
+
   render() {
     const { album } = this.props;
+    const { loading, check } = this.state;
     return (
       <div>
-        <p>{album.trackName}</p>
-        <audio data-testid="audio-component" src="{previewUrl}" controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          {' '}
-          <code>audio</code>
-        </audio>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div>
+            <p>{album.trackName}</p>
+            <audio data-testid="audio-component" src="{previewUrl}" controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              {' '}
+              <code>audio</code>
+            </audio>
+
+            <label htmlFor="check">
+              Favorita
+              <input
+                id="check"
+                data-testid={ `checkbox-music-${album.trackId}` }
+                type="checkbox"
+                checked={ check }
+                onClick={ this.onInputChecked }
+              />
+            </label>
+          </div>
+        )}
       </div>
     );
   }
@@ -22,7 +63,6 @@ MusicCard.propTypes = {
   album: PropTypes.shape({
     trackName: PropTypes.string.isRequired,
   }),
-
 }.isRequired;
 
 export default MusicCard;
